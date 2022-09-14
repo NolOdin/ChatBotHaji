110755e01947afcb3d7975b164cd9261cdd1eace
NolOdin <Mr.Ciborg@outlook.com>
2022-09-14 19:10:39 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+TOKEN = ""
+#import telebot
+# import config
+#
+# bot = telebot.TeleBot(config.TOKEN)
+#
+# @bot.message_handler(content_types=["text"])
+#
+# def coco(message):
+#      bot.send_message(message.chat.id, message.text)
+#
+# bot.polling(none_stop=True)
